933e95fd3ad0daf4f63706d0a602b5fba4334912
shaquille0520 <31034222+shaquille0520@users.noreply.github.com>
2023-12-04 19:04:41 +0000

    Add files via upload

---
+++ Rens website upload/Back_end.py
@@ -0,0 +1,46 @@
+import os
+os.system('pip install Flask playsound ')
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+from playsound import playsound
+
+
+
+os.chdir('C:\\Users\\mui\\Desktop\\Rens website upload')
+app = Flask(__name__)
+
+# Define the folder where uploaded files will be stored
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure the UPLOAD_FOLDER exists
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov', 'avi'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    playsound('C:\\Users\\mui\\Desktop\\Rens website upload\\static\\sound1.wav')
+    return render_template('index.html')
+    
+@app.route('/upload', methods=['POST']) 
+def upload(): 
+    if request.method == 'POST': 
+  
+        # Get the list of files from webpage 
+        files = request.files.getlist("file") 
+  
+        # Iterate for each file in the files List, and Save them 
+        for file in files: 
+            file.save(file.filename) 
+            os.chdir('C:\\Users\\mui\\Desktop\\Rens website upload\\uploads')
+        return render_template('thank_you.html')
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.0.4', port=5000)
